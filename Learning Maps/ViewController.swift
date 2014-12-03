1064fa7be1ff0bf23c3586a9860c9fb969f7b237
Aaron Williamson <wikea1@gmail.com>
2014-12-02 19:44:19 -0800

    Initial Commit

---
+++ Learning Maps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning Maps
+//
+//  Created by Aaron Williamson on 12/2/14.
+//  Copyright (c) 2014 Aaron Williamson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
