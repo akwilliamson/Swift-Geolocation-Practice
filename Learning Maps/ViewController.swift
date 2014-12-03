be0e21c5c6488ab644c57bdb9722a8d31cdb494a
Aaron Williamson <wikea1@gmail.com>
2014-12-02 21:50:47 -0800

    Add gesture recognizers to add pinpoint

--- Learning Maps/ViewController.swift
+++ Learning Maps/ViewController.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 45.472515, -122.683826
+        
+        var latitude: CLLocationDegrees = 45.472515
+        var longitude: CLLocationDegrees = -122.683826
+        
+        var latDelta: CLLocationDegrees = 0.01
+        var lonDelta: CLLocationDegrees = 0.01
+        
+        var span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        var location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        var region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        var annotation = MKPointAnnotation()
+        
+        annotation.coordinate = location
+        
+        annotation.title = "My House"
+        
+        annotation.subtitle = "This is where I code stuff"
+        
+        mapView.addAnnotation(annotation)
+        
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        
+        uilpgr.minimumPressDuration = 2.0
+        
+        mapView.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    func action(gestureRecognizer:UIGestureRecognizer) {
+        var touchPoint = gestureRecognizer.locationInView(self.mapView)
+        
+        var newCoordinate: CLLocationCoordinate2D = mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
+        
+        var newAnnotation = MKPointAnnotation()
+        
+        newAnnotation.coordinate = newCoordinate
+        
+        newAnnotation.title = "New point"
+        
+        newAnnotation.subtitle = "The first pointer!"
+        
+        mapView.addAnnotation(newAnnotation)
+        
     }
 
     override func didReceiveMemoryWarning() {
